4bcbfdf08020b469aee99eda974d8269ae78f8d9
hanggitas16 <hanggita.suryanti@gmail.com>
2024-11-22 17:40:46 +0700

    login by id

--- locating-elemen.py
+++ locating-elemen.py
@@ -3,6 +3,7 @@ from selenium import webdriver
 driver = webdriver.Chrome()
 
 driver.get("https://www.tokopedia.com/login")
+driver.find_element(By.ID, "input")
 
 input("Tekan enter apabila keluar dari chrome")
 
